f71ac021a9932f70b0b06cf4d8cc549409210d07
Delvison Castillo <delvisoncastillo@gmail.com>
2015-10-21 02:04:51 +0900

    tested one client and one server book commands. works. need to now implement timeouts, mutex algo, synchronization

--- LibraryClient.java
+++ LibraryClient.java
@@ -2,9 +2,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Collection;
 import java.util.Scanner;
 import java.net.Socket;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.net.UnknownHostException;
 import java.io.File;
 import java.io.PrintWriter;
@@ -13,6 +19,8 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 /**
+ * FILE: LibraryClient.java
+ *
  * Client program to be used with a distributed system modeled in
  * LibraryServer.java. Requires a special configuration file where the first
  * line indicates how many servers there are and the following lines contain the
@@ -21,6 +29,9 @@ import java.io.IOException;
  * Developed as part of a class assignment (CSE535 -- SUNY Korea)
  *
  * @author Delvison Castillo (delvison.castillo@sunykorea.ac.kr)
+ *
+ * TODO:  - Figure out clientid.
+ *				- Test client with atleast one instance of server.
  */
 
 public class LibraryClient
@@ -32,19 +43,21 @@ public class LibraryClient
 	private PrintWriter sockOut; // for writing out on the socket
 	private BufferedReader sockIn; // for reading in from the socket
 	private String configFile = ".clientConfig.dat"; // config file location
-	private HashMap<String,Boolean> servers; // < [ip:status], ...>
+	private HashMap<String,Boolean> servers; // < [addr:status], ...>
+	private String clientID;
 
 	/**
 	 * Constructor method. Initializes the hashmap used to hold the addresses of
 	 * the servers as well as their status. Calls initialize. If initialize is
 	 * successful, then the program enters its main loop.
 	 */
-	public void LibraryClient()
+	public LibraryClient()
 	{
 		servers = new HashMap<String,Boolean>();
 		if (initialize()) {
 			mainLoop();
 		} else {
+			System.out.println("No connections.");
 			terminate();
 		}
 	}
@@ -58,57 +71,79 @@ public class LibraryClient
 	{
 		try 
 		{
+			System.out.println("Initializing...");
 			f = new File(configFile);
 			Scanner s = new Scanner(f);
 			// n <- read in number of servers available
 			int servNum = s.nextInt();
+			debug("number of servers: "+servNum);
 			// add servers to hashmap of servers
-			for (int i = 0;i<servNum;i++) servers.put(s.nextLine(), true);
+			for (int i = 0;i<=servNum;i++) {
+				String j = s.nextLine();
+				if (j.contains(":")) {
+					servers.put(j, true);
+					debug("added server "+j);
+				}
+			}
 			// connect to a random server
 		  return connect() ? true : false;
 		} catch (IOException e) 
 		{
+			debug("initialize():File read error: "+configFile);
 			e.printStackTrace();
 			return false;
 		}
 	}
 
 	/**
-	 * Updates the socket with a server that is live. 
+	 * Updates the socket with a server that is live chosen at random from the
+	 * list of available servers. 
 	 * @return boolean indicating whether or not the program was able to connect 
 	 * to a server.
-	 * TODO: Randomize the server the client connects to.
 	 */
 	private boolean connect()
 	{
+		debug("Entering connect()");
 		boolean isConnected = false;
-		for (Map.Entry<String,Boolean> server : servers.entrySet())
+		this.isConnected = isConnected;
+		List<String> srvlist = new ArrayList(
+		Arrays.asList(servers.keySet().toArray()));
+		// randomize the server list
+		Collections.shuffle(srvlist);
+		for (String server : srvlist)
 		{
 			// if the server is up, attempt to connect
-			if (server.getValue() != false)
+			/* if (servers.get(server) != false) */
+			if (true) // always assume that a server is up
 			{
 				try
 				{
-					String[] s = server.getKey().split(":");
+					String[] s = server.split(":");
 					String ip = s[0];
 					int port = Integer.parseInt(s[1]);
 					host = new Socket(ip,port);
+					debug("connect(): socket created on port "+host.getLocalPort());
 					sockOut = new PrintWriter(host.getOutputStream(),true);
 					sockIn = new BufferedReader(
 					new InputStreamReader(host.getInputStream()));
 					// receive ACK.
-					if (sockIn.readLine() != null) isConnected = true;
-				} catch (UnknownHostException e) 
+					debug("connect(): attempting to connect to "+ip+":"+port);
+					char[] buffer = new char[64];
+					int r = sockIn.read(buffer,0,64);
+					if (r != 0) 
+					{
+						clientID = new String(buffer);
+						isConnected = true;
+						System.out.println("Connected to ... "+getIP(host)+" as "+clientID);
+					} else {
+						System.out.println("Failed to connect to... "+getIP(host));
+					}
+				} catch (Exception e) 
 				{
-					isConnected = false;
-					servers.put(server.getKey(),false);
-					e.printStackTrace();
-				} catch (IOException e)
-				{
-					isConnected = false;
-					servers.put(server.getKey(),false);
-					e.printStackTrace();
-				}
+					debug("connect():Exception on "+server);
+					servers.put(server,false);
+					/* e.printStackTrace(); */
+				} 
 			}
 			if (isConnected) break;
 		}
@@ -117,7 +152,7 @@ public class LibraryClient
 	}
 
 	/**
-	 * Sends a command to the server. 
+	 * Sends a command to the server, receives a response. 
 	 * @param String cmd - command to be sent to the server.
 	 * @return String containing the response from the server.
 	 */
@@ -129,14 +164,20 @@ public class LibraryClient
 			try 
 			{
 				sockOut.println(cmd);
-				res = sockIn.readLine();
+				debug("sent command: "+cmd);
+				char[] buffer = new char[64];
+				int r = sockIn.read(buffer,0,buffer.length);
+				res = new String(buffer);
+				debug("received :"+res);
 			} catch(IOException e) 
 			{
 				e.printStackTrace();
-				servers.put(host.getInetAddress().getHostAddress(), false);
+				/* servers.put(host.getInetAddress().getHostAddress(), false); */
 				return connect() ? sendCmd(cmd) : "SYSTEM IS DOWN.";
 			}
-		} else { res = "ERROR: No connection."; }
+		} else { 
+			res = "ERROR: No connection."; 
+		}
 		return res;
 	}
 
@@ -145,20 +186,23 @@ public class LibraryClient
 	 */
 	private void mainLoop()
 	{
+		debug("entering mainLoop().");
 		Scanner in = new Scanner(System.in);
 		String cmd = "";
-		while (!((cmd = in.next()).equals("quit")))
+		prompt();
+		while (!((cmd = in.nextLine()).equals("quit")))
 		{
-			prompt();
 			sendCmd(cmd);
+			prompt();
 		}
 	}
 
 	/**
 	 * Terminates the program.
 	 */
-	public void terminate()
+	private void terminate()
 	{
+		debug("Terminating...");
 		try
 		{
 			host.close();
@@ -179,11 +223,28 @@ public class LibraryClient
 	}
 
 	/**
+	 * Takes in a socket object and returns the IP of that socket.
+	 * @param Socket sock - desired socket to get the IP for.
+	 * @return String ip address of the socket.
+	 */
+	private String getIP(Socket sock)
+	{
+		SocketAddress s = sock.getRemoteSocketAddress();
+		int port = ((InetSocketAddress) s).getPort();
+		return sock.getInetAddress().getHostAddress()+":"+port;
+	}
+
+	/**
 	 * Used to print debug messages.
 	 * @param String msg - debug message to be printed out
 	 */
 	private void debug(String msg)
 	{
-		if (debug) System.out.println("[*] DEBUG: "+msg);
+		if (this.debug) System.out.println("[*] DEBUG: "+msg);
+	}
+
+	public static void main(String[] args)
+	{
+		LibraryClient c1= new LibraryClient();
 	}
 }
--- LibraryServer.java
+++ LibraryServer.java
@@ -6,15 +6,17 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.Iterator;
 import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.net.SocketAddress;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.Selector;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ClosedChannelException;
 import java.nio.ByteBuffer;
-import java.net.ServerSocket;
-import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
 import java.io.File;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
@@ -22,18 +24,36 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 
 /**
+ * FILE: LibraryServer.java
+ *
  * Server program meant to operate within a distributed system. Uses Lamport's
  * Mutual Exclusion Algorithm. Distributed system's function is replication.
- * Models a library system. Uses a configuration file 
+ * Models a library system. Uses a configuration file where the first line has 2
+ * space seperated values n and z. n = number of servers. z = number of books in
+ * the library. The id's of the book are preceded with a b followed by an
+ * integer value. The next n number of lines contain the addresses to the
+ * servers in the distributed system followed by a port number such as
+ * 127.0.0.1:1234. The rest of the file will have 3 values per each line. The
+ * first value will have the server id. The 2nd value is a non-negative integer
+ * that tells the server to become unresponsive after receiving the kth message.
+ * The 3rd value contains a duration in milliseconds for which the given server
+ * would become unresponsive - to all clients and servers.
  *
  * Developed as part of a class assignment (CSE535 -- SUNY Korea)
  *
  * @author Delvison Castillo (delvison.castillo@sunykorea.ac.kr)
+ *
+ * TODO:  - set up lamports mutex algorithm
+ *				- set up system of ACKs in between servers when they connect.
+ *				- set up synchronize message between servers
+ *				- test with atleast one client
+ *				- initialize books from the file
+ *				- implement timeout feature. extract own timeout value from configFile
  */
 public class LibraryServer
 
 {
-	private int port = 3000;
+	private int port; // port number for the server to listen to
 	private boolean debug = true; // debug flag
 	private HashMap<String,Boolean> servers; // < [addr:status], ...>
 	private ArrayList<SocketChannel> clients_sock; // client SocketChannels
@@ -43,19 +63,25 @@ public class LibraryServer
 	private File f; // configuration file object
 	private int clock; // lamport timestamp
 	private Selector selector; // for asynchronous I/O
+	private String[][] books; // data structure for books
+	private int bookNum; // number of books in the library
+	private int timeout; // time in milliseconds to become unresponsive
+	private int clientCount;
 
 	/**
 	 * Constructor method. Initializes servers and clients data structures, calls
 	 * initialize method, enters the programs mainloop in initialize is
 	 * successful.
 	 */
-	public void LibraryServer()
+	public LibraryServer(int port)
 	{
+		this.port = port;
+		clientCount = 0;
 		clock = 0;
 		servers = new HashMap<String,Boolean>();
 		clients_sock = new ArrayList<SocketChannel>();
 		servers_sock = new ArrayList<SocketChannel>();
-		if (initialize()) {
+		if (initialize() && synchronize()) {
 			mainLoop();
 		} else {
 			terminate();
@@ -70,20 +96,39 @@ public class LibraryServer
 	{
 		try 
 		{
+			System.out.println("Initializing...127.0.0.1:"+port);
 			// create socket
 			serverSocketCh = ServerSocketChannel.open();
-			serverSocketCh.socket().bind(new InetSocketAddress(port));
-			// create selector
-			Selector selector = Selector.open();
-			SelectionKey key =serverSocketCh.register(
-			selector, (SelectionKey.OP_READ | SelectionKey.OP_WRITE 
-			| SelectionKey.OP_CONNECT | SelectionKey.OP_ACCEPT));
+			serverSocketCh.bind(new InetSocketAddress(port));
+			serverSocketCh.configureBlocking(false);
+			// create selector (NIO)
+			selector = Selector.open();
+			/* int interestSet = (SelectionKey.OP_READ | SelectionKey.OP_WRITE  */
+			/* | SelectionKey.OP_CONNECT | SelectionKey.OP_ACCEPT); */
+			int interestSet = serverSocketCh.validOps();
+			SelectionKey key =serverSocketCh.register(selector, interestSet);
+			// read config file
 			f = new File(configFile);
 			Scanner s = new Scanner(f);
 			// n <- read in number of servers available
 			int servNum = s.nextInt();
+			debug("amount of servers: "+servNum);
+			this.bookNum = s.nextInt();
+			debug("amount of books: "+bookNum);
+			books = new String[bookNum][3];
+			// populate books
+			for (int i=0;i<bookNum;i++){
+				books[i][0] = "b"+i;
+				books[i][1] = "free";
+			}
 			// add servers to hashmap of servers
-			for (int i = 0;i<servNum;i++) servers.put(s.nextLine(), true);
+			for (int i = 0;i<servNum;i++) {
+				String j = s.nextLine();
+				if (j.contains(":")) {
+					debug("read server from config: "+j);
+					servers.put(j, true);
+				}
+			}
 			// connect to servers
 		  return connectToServers() ? true : false;
 		} catch (IOException e) 
@@ -109,19 +154,24 @@ public class LibraryServer
 				SocketChannel sock = SocketChannel.open();
 				sock.configureBlocking(false);
 				sock.connect(new InetSocketAddress(ip,port));
-				// write ack to socketchannel
+				// write ACK to socketchannel
 				if (!send(sock, "ACK")) servers.put(server.getKey(), false);
+				// add socketchannel to the arraylist
 				servers_sock.add(sock);
-				SelectionKey key =sock.register(
-				selector, (SelectionKey.OP_READ | SelectionKey.OP_WRITE
-				| SelectionKey.OP_CONNECT | SelectionKey.OP_ACCEPT));
+				// register the socketchannel to the selector (NIO)
+				SelectionKey key =sock.register(selector,sock.validOps());
+				/* selector, (SelectionKey.OP_READ | SelectionKey.OP_WRITE */
+				/* | SelectionKey.OP_CONNECT | SelectionKey.OP_ACCEPT)); */
+				debug("Added SocketChannel: "+getIP(sock));
 			} catch (UnknownHostException e) 
 			{
+				debug("connectToServers():UnknownHostException "+server.getKey());
 				servers.put(server.getKey(),false);
 				e.printStackTrace();
 				return false;
 			} catch (IOException e)
 			{
+				debug("connectToServers():IOException.");
 				servers.put(server.getKey(),false);
 				e.printStackTrace();
 				return false;
@@ -138,22 +188,35 @@ public class LibraryServer
 	 */
 	private boolean send(SocketChannel sock,String msg) 
 	{
-		try
+		if(!getIP(sock).equals("127.0.0.1:"+serverSocketCh.socket().getLocalPort())) 
 		{
-			final byte[] by = msg.getBytes();
-			ByteBuffer b = ByteBuffer.allocate(by.length);
-			b.put(by);
-			b.flip();
-			while(b.hasRemaining()){
-				sock.write(b);
+			try
+			{
+				final byte[] by = msg.getBytes();
+				ByteBuffer b = ByteBuffer.allocate(by.length);
+				b.put(by);
+				b.flip();
+				while(b.hasRemaining()){
+					int p = sock.write(b);
+					debug("send():sent bytes: "+p);
+				}
+				debug("send():Sent message to SocketChannel: "+getIP(sock)+": "+msg);
+				return true;
+			} catch (IOException e)
+			{
+				debug("send():IOException sending to "+getIP(sock));
+				/* e.printStackTrace(); */
+				String ip = sock.socket().getInetAddress().getHostAddress();
+				if (servers.containsKey(ip)) servers.put(ip,false);
+				return false;
+			} catch (java.nio.channels.NotYetConnectedException e)
+			{
+				debug("send():Not yet connected to "+getIP(sock));
+				/* e.printStackTrace(); */
+				return false;
 			}
+		} else {
 			return true;
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-			String ip = sock.socket().getInetAddress().getHostAddress();
-			if (servers.containsKey(ip)) servers.put(ip,false);
-			return false;
 		}
 	}
 
@@ -169,17 +232,22 @@ public class LibraryServer
 	{
 			try
 			{
-				if (send(clientCh, "ACK"))
+				if (send(clientCh, "c"+clientCount))
 				{
 					clients_sock.add(clientCh);
-					SelectionKey key =clientCh.register(
-					selector, (SelectionKey.OP_READ | SelectionKey.OP_WRITE
-					| SelectionKey.OP_CONNECT | SelectionKey.OP_ACCEPT));
+					clientCh.configureBlocking(false);
+					SelectionKey key =clientCh.register(selector,clientCh.validOps());
+					clientCount++;
 					return true;
 				} else {
 					return false;
 				}
 			} catch (ClosedChannelException e){
+				debug("connectClient():ClosedChannelException.");
+				e.printStackTrace();
+				return false;
+			} catch (IOException e){
+				debug("connectClient():ClosedChannelException.");
 				e.printStackTrace();
 				return false;
 			}
@@ -192,8 +260,9 @@ public class LibraryServer
 	 */
 	private String recv(SocketChannel sock) 
 	{
+		String ip = getIP(sock);
+		debug("recv(): Entering with "+ip);
 		String recv = "";
-		String ip = sock.socket().getInetAddress().getHostAddress();
 		try
 		{
 			if (!servers.containsKey(ip))
@@ -203,13 +272,23 @@ public class LibraryServer
 				int bytesRead = sock.read(buf);
 				buf.flip();
 				recv = new String(buf.array());
+				String[] cmd = recv.trim().split(" ");
+				debug("recv(): received "+recv);
+				if (cmd.length == 3){
+					send(sock, processBook(cmd[1],cmd[2],cmd[0]));
+				} else {
+					send(sock, "Invalid command.");	
+				}
 			} else{
-				// TODO: receiving from another server
+				// TODO: receiving from server.
+				// TODO: check for connect ACK. if so, open socket and change status.
 			}
 		} catch (IOException e)
 		{
+			debug("recv():IOException.");
 			e.printStackTrace();
 		}
+		debug("recv(): Exiting with "+ip);
 		return recv;
 	}
 
@@ -218,10 +297,10 @@ public class LibraryServer
 	 * @return Boolean indicating whether or not the synchronization was
 	 * successful.
 	 */
-	private void synchronize()
+	private boolean synchronize()
 	{
 		// TODO: Write me!
-
+		return true;
 	}
 
 	/**
@@ -232,6 +311,31 @@ public class LibraryServer
 	{
 		// TODO: Write me!
 		return true;
+	}
+
+	private String processBook(String clientID, String bookID, String cmd)
+	{
+		debug("processBook():"+cmd+" "+clientID+" "+bookID);
+		for (int i=0;i<bookNum;i++)
+		{
+			if (books[i][0].equals(bookID))
+			{
+				debug("processBook(): book found");
+				if (cmd.equals("reserve") && books[i][1].equals("free")){
+					books[i][1] = "reserved";
+					books[i][2] = clientID;
+					return clientID+" "+bookID;
+				}
+
+				if (cmd.equals("return") && books[i][1].equals("reserved") 
+					&& books[i][2].equals(clientID)){
+					books[i][1] = "free";
+					books[i][2] = null;
+					return "free "+clientID+" "+bookID;
+				}
+			}
+		}
+		return "fail "+clientID+" "+bookID;
 	}
 
 	/**
@@ -249,42 +353,53 @@ public class LibraryServer
 	 */
 	private void mainLoop()
 	{
+		debug("Entering mainLoop()");
 		try 
 		{
-			// SelectionKey key = channel.register(selector, SelectionKey.OP_READ);
-			Set<SelectionKey> selectedKeys = selector.selectedKeys();
-			Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
 			String cmd = "";
 			SocketChannel sc = null;
 			while (true)
 			{
-				SelectionKey key = keyIterator.next();
+				selector.select();
+				Set<SelectionKey> selectedKeys = selector.selectedKeys();
+				Iterator<SelectionKey> keyIterator = selectedKeys.iterator();
+				while (keyIterator.hasNext())
+				{
+					SelectionKey key = keyIterator.next();
 
-				if(key.isAcceptable()) 
-				{
-					// a connection was accepted by a ServerSocketChannel.
-					sc = (SocketChannel)key.channel();
-				} else if (key.isConnectable()) 
-				{
-					// a connection was established with a remote server.
-					sc = (SocketChannel)key.channel();
+					if(key.isAcceptable()) 
+					{
+						// a connection was accepted by a ServerSocketChannel.
+						ServerSocketChannel ssc = (ServerSocketChannel)key.channel();
+						/* debug("Socketchannel is acceptable: "); */
+						connectClient(ssc.accept());
+					} else if (key.isConnectable()) 
+					{
+						// a connection was established with a remote server.
+						sc = (SocketChannel)key.channel();
 
-				} else if (key.isReadable()) 
-				{
-					// a channel is ready for reading
-					sc = (SocketChannel)key.channel();
-					recv(sc);
+					} else if (key.isReadable()) 
+					{
+						// a channel is ready for reading
+						sc = (SocketChannel)key.channel();
+						debug("Socketchannel is readable: "+getIP(sc));
+						// receive data on SocketChannel
+						recv(sc);
+					} else if (key.isWritable()) 
+					{
+						// a channel is ready for writing
+						sc = (SocketChannel)key.channel();
+						/* debug("Socketchannel is writable: "+getIP(sc)); */
+						// write data on SocketChannel
+					}
 
-				} else if (key.isWritable()) 
-				{
-					// a channel is ready for writing
-					sc = (SocketChannel)key.channel();
+					// remove key from selected keys after processing.
+					keyIterator.remove();
 				}
-
-				keyIterator.remove();
 			}
 		} catch (Exception e) 
 		{
+			debug("mainLoop(): Exception ");
 			e.printStackTrace();
 			terminate();
 		}
@@ -298,16 +413,54 @@ public class LibraryServer
 	{
 		try
 		{
+			// close all server sockets
 			for (SocketChannel sock : servers_sock){
+				System.out.println("Closing server connection to..."+getIP(sock));
 				sock.close();
 			}
+			// close all client sockets 
 			for (SocketChannel sock : clients_sock){
+				System.out.println("Closing client connection to..."+getIP(sock));
 				sock.close();
 			}
+			// close listening socket
 			serverSocketCh.close();
+			System.out.println("Program terminated.");
 		} catch (IOException e) {
+			debug("terminate():IOException.");
 			System.exit(0);
 		}
 		System.exit(0);
 	}
+
+	/**
+	 * Returns the IP address and port of of a given SocketChannel.
+	 * @param SocketChannel sock - the desired SocketChannel to the ip address for
+	 * @return String IP of the SocketChannel given.
+	 */
+	private String getIP(SocketChannel sock)
+	{
+		Socket socket = sock.socket();
+		/* SocketAddress s = socket.getRemoteSocketAddress(); */
+		/* int port = ((InetSocketAddress) s).getPort(); */
+		int port = socket.getPort();
+		return sock.socket().getInetAddress().getHostAddress()+":"+port;
+	}
+
+	/**
+	 * Used to print debug messages.
+	 * @param String msg - debug message to be printed out
+	 */
+	private void debug(String msg)
+	{
+		if (debug) System.out.println("[*] DEBUG: "+msg);
+	}
+
+
+
+	public static void main(String[] args)
+	{
+		LibraryServer s1= new LibraryServer(3001);
+	}
 }
+
